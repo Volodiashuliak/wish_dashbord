5ecc0a3738e1a6211f3727bb5b6ed36bc8bcdc84
Volodiashuliak <117200215+Volodiashuliak@users.noreply.github.com>
2022-11-08 18:31:57 +0200

    Add files via upload

---
+++ js/data.js
@@ -0,0 +1,45 @@
+const user = {
+  name: "Володимир ",
+  surname: "Шуляк"  : ".WIN_20220222_09_00_53_Pro.jpgjpg",
+  qrQode: "qr-code.svg",
+  dreams: [
+    { src: "car.jpg", alt: "Авто моєї мрії" },
+    { src: "home.jpg", alt: "Домівка моеї мрії" },
+    { src: "voyage.jpg", alt: "Подорож до Марсу" },
+  ],
+};
+
+// arr це середня місячна платня по рокам
+const arr = [
+  500,
+  700,
+  900,
+  1100,
+  1300,
+  1500,
+  1700,
+  1900,
+  2100,
+  2300
+  2500,
+  2700,
+];
+
+const necessaryExpenses = 800;
+
+const todo = [
+  { month: "Червень", skill: "JavaScript" },
+  { month: "Липень", skill: "Java" },
+  { month: "Серпень", skill: "Python" },
+  { month: "Вересень", skill: "Самостійність" },
+  { month: "Жовтень", skill: "Гнучкість" },
+  { month: "Листопад", skill: "Емоційний інтелект" },
+  { month: "Грудень", skill: "Креативність" },
+  { month: "Січень", skill: "Лідерство" },
+  { month: "Лютий", skill: "Нетворкінг" },
+  { month: "Березень", skill: "Особистий бренд" },
+  { month: "Квітень", skill: "Управління фінансами" },
+  { month: "Травень", skill: "Публічні виступи" },
+];
+
+export { user, arr, necessaryExpenses, todo };
